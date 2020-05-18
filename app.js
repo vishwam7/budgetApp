752aad2a8cbcf363cfea55b2beb12ff1eb5ea64d
[Vishwam Shah] <[shahvishwam7@gmail.com]>
2020-05-18 14:06:56 +0530

    feature3:addItem focus toggle

--- app.js
+++ app.js
@@ -174,6 +174,13 @@ var UIController = (function() {
 
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
+
+    var nodeListForeEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function() {
             return {
@@ -235,11 +242,6 @@ var UIController = (function() {
         displayPercentages: function(percentages) {
 
             var fields = document.querySelectorAll(DOMStrings.expensesPercentageLabel);
-            var nodeListForeEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
             nodeListForeEach(fields, function(current, index) {
                 //do stuff
                 if (percentages[index] > 0)
@@ -247,7 +249,6 @@ var UIController = (function() {
                 else
                     current.textContent = '---';
             });
-
         },
 
         displayMonth: function() {
@@ -257,6 +258,18 @@ var UIController = (function() {
             month = now.getMonth();
             year = now.getFullYear();
             document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
+        changedType: function() {
+            var fields = document.querySelectorAll(
+                DOMStrings.inputType + ',' +
+                DOMStrings.inputDescription + ',' +
+                DOMStrings.inputValue);
+
+            nodeListForeEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+            document.querySelector(DOMStrings.inputBtn).classList.toggle('red');
         },
         getDOMStrings: function() {
             return DOMStrings;
@@ -278,6 +291,8 @@ var controller = (function(budgetCtrl, UICtrl) {
             }
         });
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
     var updateBudget = function() {
         //1.calculate budget
